1a24a574c6eb31f995ad3225e42e351e61e8f1c6
esandoval30 <esandoval@salesforce.com>
2020-02-24 11:45:48 +0000

    Update orgInit.sh
    getting exec envrionment info

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,10 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
-sfdx force:source:push
-sfdx force:user:password:generate
-sfdx force:org:open -p /lightning/setup/SetupOneHome/home
+cat /etc/os-release
+uname -r
+sfdx --version
+heroku --version
+openssl version
+node --version
+#sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
+#sfdx force:source:push
+#sfdx force:user:password:generate
+#sfdx force:org:open -p /lightning/setup/SetupOneHome/home
